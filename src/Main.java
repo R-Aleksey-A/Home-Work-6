0a6d1c5de23c4a5b6e63825de862f26ba71e3825
huawei <romanec.aleksey@gmail.com>
2024-11-28 20:48:15 +0400

    HW6.2

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,14 @@ public class Main {
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
+        {
+            System.out.println(
+                    "Задание-2"
+            );
+        }
+        for (int i=10; i>0; i--) {
 
+            System.out.println(i);
+        }
     }
 }
